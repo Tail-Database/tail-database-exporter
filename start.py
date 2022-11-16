8d486633e436a9ac1fef79071bf51759e849e316
Freddie Coleman <f.coleman@hotmail.co.uk>
2022-11-16 14:21:26 +0000

    search index points to hashes and tails

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
 import jsonpickle
-from typing import Dict
+from typing import Dict, List
 from config import Config
 from tail import Tail
 from tail_database import TailDatabase
@@ -7,7 +7,8 @@ from tail_database import TailDatabase
 if Config.output_directory is None:
     raise Exception("TAILDB_EXPORTER_OUTPUT_DIR must be set")
 
-search_index: Dict[str, Tail] = {}
+search_index: Dict[str, str] = {}
+tails: List[Tail] = []
 
 tail_database = TailDatabase()
 
@@ -27,9 +28,10 @@ for tail in tail_database.tails():
         nft_uri
     )
 
-    search_index[tail.hash] = tail
-    search_index[tail.code] = tail
-    search_index[tail.name] = tail
+    tails.append(tail)
+    search_index[tail.hash] = tail.hash
+    search_index[tail.code] = tail.hash
+    search_index[tail.name] = tail.hash
 
     tail_json = jsonpickle.encode(tail, unpicklable=False)
 
@@ -37,6 +39,10 @@ for tail in tail_database.tails():
         f.write(tail_json)
 
 search_index_json = jsonpickle.encode(search_index, unpicklable=False)
+tails_json = jsonpickle.encode(tails, unpicklable=False)
 
 with open(Config.output_directory + f"/search_index.json", 'w+') as f:
     f.write(search_index_json)
+
+with open(Config.output_directory + f"/tails.json", 'w+') as f:
+    f.write(tails_json)
